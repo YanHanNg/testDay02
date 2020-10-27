aee4878aae51d1fb807ade9f9b24175cfbbe8ea6
ngyanhan <yanhanng89@gmail.com>
2020-10-27 20:02:41 +0800

    My Second Commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.engine('hbs', hbs({defaultLayout: 'default.hbs'}));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.status(200);
+    res.type('text/html');
+    res.render('2dice');
+})
+
+let dice1 = 0;
+let dice2 = 0;
+
+app.get('/roll', (req, res) => {
+    dice1 = Math.floor(Math.random() * 6) + 1;
+    dice2 = Math.floor(Math.random() * 6) + 1;
+    res.status(200);
+    res.type('text/html');
+    res.render('rolldice', {
+        dice1Value: dice1,
+        dice2Value: dice2
+    })
+})
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res) => {
+    res.status(200);
+    res.type('text/html');
+    res.render('2dice');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server Started on ${PORT} at ${new Date()}`);
+})
