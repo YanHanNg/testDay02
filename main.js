e416dd6cfe6c54680fc65ce7618a5d075f2c3462
ngyanhan <yanhanng89@gmail.com>
2020-10-28 09:53:57 +0800

    complete-dice-roll

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const PORT = parseInt(process.env.PORT) || 3000;
 app.engine('hbs', hbs({defaultLayout: 'default.hbs'}));
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
+app.get(['/', '/index.html'], (req, res) => {
     res.status(200);
     res.type('text/html');
     res.render('2dice');
@@ -16,23 +16,28 @@ app.get('/', (req, res) => {
 let dice1 = 0;
 let dice2 = 0;
 
+const roll_dice = () => Math.floor(Math.random() * 6) + 1;
+
+const DICE_IMG = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+
 app.get('/roll', (req, res) => {
-    dice1 = Math.floor(Math.random() * 6) + 1;
-    dice2 = Math.floor(Math.random() * 6) + 1;
+    dice1 = roll_dice();
+    dice2 = roll_dice();
     res.status(200);
     res.type('text/html');
     res.render('rolldice', {
-        dice1Value: dice1,
-        dice2Value: dice2
+        dice1Value: DICE_IMG[dice1-1],
+        dice2Value: DICE_IMG[dice2-1]
     })
 })
 
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res) => {
-    res.status(200);
-    res.type('text/html');
-    res.render('2dice');
+    // res.status(200);
+    // res.type('text/html');
+    // res.render('2dice');
+    res.redirect('/');
 })
 
 app.listen(PORT, () => {
